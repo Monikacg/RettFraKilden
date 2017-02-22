e7e3e8d40c066593742a4310f3cbfc5cd2ca01ff
Arktoz <bjorakr@stud.ntnu.no>
2017-02-22 13:23:38 +0100

    Add files via upload

--- Heisprosjekt/admin/lift_properties/lift_properties.go
+++ Heisprosjekt/admin/lift_properties/lift_properties.go
@@ -1,44 +1,44 @@
 package lift_properties
 
 import (
-  . "./../../definitions"
+	. "./../../definitions"
 )
 
-func Create_lift_prop_list() ([]int) {
-  prop_list := make([]int, 3*MAX_N_LIFTS)
-  for i := 0; i < MAX_N_LIFTS; i++ {
-     prop_list[3*i] = NOT_VALID // Last floor (not valid) Kan godt ha -2 som ikke gyldig på begge
-     prop_list[3*i+1] = NOT_VALID // Direction (not valid) Ide: Legg til def av not valid?
-     prop_list[3*i+2] = INIT // State (INIT)
-   }
-  return prop_list
+func Create_lift_prop_list() []int {
+	prop_list := make([]int, 3*MAX_N_LIFTS)
+	for i := 0; i < MAX_N_LIFTS; i++ {
+		prop_list[3*i] = NOT_VALID   // Last floor
+		prop_list[3*i+1] = NOT_VALID // Direction
+		prop_list[3*i+2] = INIT      // State
+	}
+	return prop_list
 }
 
-func Set_last_floor(properties []int, lift, last_floor int)  {
-  properties[3*lift] = last_floor
+func Set_last_floor(properties []int, lift, last_floor int) {
+	properties[3*lift] = last_floor
 }
 
-func Set_dirn(properties []int, lift, dirn int)  {
-  properties[3*lift+1] = dirn
+func Set_dirn(properties []int, lift, dirn int) {
+	properties[3*lift+1] = dirn
 }
 
-func Set_state(properties []int, lift, state int)  {
-  properties[3*lift+2] = state
+func Set_state(properties []int, lift, state int) {
+	properties[3*lift+2] = state
 }
 
-func Get_properties(properties []int, lift int) (Properties_struct) {
-  return Properties_struct{Last_floor: properties[3*lift], Dirn: properties[3*lift+1], State: properties[3*lift+2]}
+func Get_properties(properties []int, lift int) Properties_struct {
+	return Properties_struct{Last_floor: properties[3*lift], Dirn: properties[3*lift+1], State: properties[3*lift+2]}
 } // Tror ikke den her trengs/skal brukes. Sender hel tabell når slår opp.
 // Alternativet er å oppdatere en struct hele tiden i tillegg.
 
 func Get_last_floor(properties []int, lift int) int { // I calculate_order
-  return properties[3*lift]
+	return properties[3*lift]
 }
 
 func Get_dirn(properties []int, lift int) int { // I calculate_order
-  return properties[3*lift+1]
+	return properties[3*lift+1]
 }
 
 func Get_state(properties []int, lift int) int { // I admin, calculate_order
-  return properties[3*lift+2]
+	return properties[3*lift+2]
 }
--- Heisprosjekt/definitions/definitions.go
+++ Heisprosjekt/definitions/definitions.go
@@ -8,56 +8,56 @@ weakness in the system.
 */
 
 const (
-  // Number of floors/buttons
-  N_FLOORS = 4
-  N_BUTTONS = 3
-  MAX_N_LIFTS = 3
+	// Number of floors/buttons
+	N_FLOORS    = 4
+	N_BUTTONS   = 3
+	MAX_N_LIFTS = 3
 
-  // Lift states
-  INIT = -1
-  IDLE = 0
-  MOVING = 1
-  DOOR_OPEN = 2
+	// Lift states
+	INIT      = -1
+	IDLE      = 0
+	MOVING    = 1
+	DOOR_OPEN = 2
 
-  // Button calls
-  BUTTON_CALL_UP = 0
-  BUTTON_CALL_DOWN = 1
-  BUTTON_COMMAND = 2
+	// Button calls
+	BUTTON_CALL_UP   = 0
+	BUTTON_CALL_DOWN = 1
+	BUTTON_COMMAND   = 2
 
-  // Motor directions
-  DIRN_DOWN = -1
-  DIRN_STOP = 0
-  DIRN_UP = 1
+	// Motor directions
+	DIRN_DOWN = -1
+	DIRN_STOP = 0
+	DIRN_UP   = 1
 
-  NOT_VALID = -2
+	NOT_VALID = -2
 
-  ON = 1
-  OFF = 0
+	ON  = 1
+	OFF = 0
 )
 
 type Button struct { // Brukes på button_outside_chan. Kanskje endre til å bruke DIRN_UP og DIRN_DOWN?
-  Floor int // Kan kutte ned til 1 button channel.
-  Button_dir int
+	Floor      int // Kan kutte ned til 1 button channel.
+	Button_dir int
 }
 
 type Order struct { // Brukes på local_order_chan
-  Cat string // "LIGHT"/"DIR"
-  Order int  // DIRN_DOWN/UP/STOP, BUTTON_CALL_UP/DOWN/COMMAND
-  Floor int //0-3 (0-N_FLOORS)
-  Value int // ON/OFF for lys, settes bare for "LIGHT"
+	Cat   string // "LIGHT"/"DIR"
+	Order int    // DIRN_DOWN/UP/STOP, BUTTON_CALL_UP/DOWN/COMMAND
+	Floor int    //0-3 (0-N_FLOORS)
+	Value int    // ON/OFF for lys, settes bare for "LIGHT"
 } // Floor trengs ikke på doorlight, value trengs ikke på retn.
 
 type Properties_struct struct { // Brukes i get_properties. ENDRE NAVN!
-  Last_floor int
-  Dirn int
-  State int
+	Last_floor int
+	Dirn       int
+	State      int
 }
 
 type Udp struct {
-  id int
-  message string
-  info string
-  Order
+	id      int
+	message string
+	info    string
+	Order
 }
 
 // NB! GÅ gjennom og endre til stor bokstav i koden også.
